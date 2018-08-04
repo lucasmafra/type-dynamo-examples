30388220d78ea8320c683c4f5eaf9ee9a765cced
Lucas Mafra <lucas.mafra95@gmail.com>
2018-08-04 16:29:48 -0300

    changed createTodo to always set initial status as ACTIVE

--- serverless-todo-application/server/src/api/handlers.ts
+++ serverless-todo-application/server/src/api/handlers.ts
@@ -2,10 +2,11 @@ import { APIGatewayEvent, Handler } from 'aws-lambda'
 import { TodoRepository } from '../db/todo'
 import { v4 as generateId } from 'uuid'
 import { buildResponse } from './build-response'
+import { TodoStatus } from '../models/todo'
 
 export const createTodo: Handler = async (event: APIGatewayEvent) => {
-  const { name, status } = JSON.parse(event.body)
-  const { data: todo } = await TodoRepository.save({ id: generateId(), name, status }).execute()
+  const { name } = JSON.parse(event.body)
+  const { data: todo } = await TodoRepository.save({ id: generateId(), name, status: TodoStatus.ACTIVE }).execute()
   return buildResponse(200, 'Todo created with success!', todo)
 }
 
